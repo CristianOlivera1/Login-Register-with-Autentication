770cc5ce35402d53d893a9be8c7b1866f5655170
cristianolivera1 <oliverachavezcristian@gmail.com>
2026-01-27 20:24:15 -0500

    fix: Clean up HTML structure and conditionally load scripts based on current path

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Codeoner</title> 
+    <title>Codeoner</title>
     <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 
@@ -17,11 +17,25 @@
     </main>
 
     <?php include __DIR__ . '/../partials/footer.php'; ?>
-    
+
     <?php include __DIR__ . '/../partials/auth-modal.php'; ?>
 
-    <script src="/assets/js/spa.js"></script>
+    <?php
+    $currentPath = $_SERVER['REQUEST_URI'];
+    $isPublicCV = preg_match('/^\/CV\/[a-z0-9\-]+$/', $currentPath);
+    $isGenerator = strpos($currentPath, '/generate-cv') !== false;
+    ?>
+
+    <?php if (!$isPublicCV): ?>
+        <script src="/assets/js/spa.js"></script>
+    <?php endif; ?>
+
     <script src="/assets/js/auth.js"></script>
+    <script src="/assets/js/custom-select.js" defer></script>
+
+    <?php if ($isGenerator): ?>
+        <script src="/assets/js/cv-generator.js" defer></script>
+    <?php endif; ?>
     <script src="/assets/js/iconify-icon.min.js"></script>
 </body>
 
